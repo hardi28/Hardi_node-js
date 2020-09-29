87ebe4b2bf30fa6f3b07c972b28f7c5c8f54e67e
Hardi <hardi.technotery@gmail.com>
2020-09-29 19:23:22 +0530

    Tranaction (err:Not debited)

--- sequential_table_entries/back_end/api/appApi.js
+++ sequential_table_entries/back_end/api/appApi.js
@@ -108,35 +108,194 @@ var session =require ('session');
 
 // ------------------------------------------------------ TRANSACTION INTO TABLES-------------------------------------------------------------
 
+// app.post('/transaction' ,function(request,res) {
+    
+//    MongoClient.connect(mongoDbURL,  { useUnifiedTopology: true }, function(err, dbClient) {
+    
+//     var dataBaseName ='test';
+//     var dbCollection = dbClient.db(dataBaseName).collection('accounts');
+//     dbCollection.insert([
+//       { _id: "har", balance: 500, pendingTransactions: [] },
+//       { _id: "dave", balance:500, pendingTransactions: [] }
+      
+//     ], function(error, result) {
+//       if (result == 0) {
+//         console.log(result);
+//             const dbCollectTransaction = dbClient.db(dataBaseName).collection('transactions');
+//               dbCollectTransaction.insert(
+//                 { _id: 4 , source: "har", destination: "dave", value:200, state: "initial", lastModified: new Date() }
+//             )
+          
+//         }
+//         else {
+//           const dbCollectTransaction = dbClient.db(dataBaseName).collection('transactions');
+//           var t = dbCollectTransaction.findOne( { state: "initial" } );
+//           //  console.log(t)
+//             dbCollectTransaction.updateOne(
+//             { _id: 4, state: "initial" },
+//             {
+//               $set: { state: "pending" },
+//               $currentDate: { lastModified: true }
+//             }
+//           )
+//         }
+        
+        
+//           dbCollection.updateOne(
+//             { _id: "har", pendingTransactions: { $ne: t._id } },
+//             { $inc: { balance: -200}, $push: { pendingTransactions: t._id } }
+//             )
+//              dbCollection.updateOne(
+//               { _id: "dave", pendingTransactions: { $ne: t._id } },
+//               { $inc: { balance: t.value }, $push: { pendingTransactions: t._id } }
+//            )
+           
+//            const dbCollectTransaction = dbClient.db(dataBaseName).collection('transactions');
+
+//         dbCollectTransaction.update(
+//         { _id: 4, state: "pending" },
+//         {
+//           $set: { state: "applied" },
+//           $currentDate: { lastModified: true }
+//         }
+//      )
+//       });
+      
+      
+  
+//   });
+  
+//   });
+
 app.post('/transaction' ,function(request,res) {
-    
-   MongoClient.connect(mongoDbURL,  { useUnifiedTopology: true }, function(err, dbClient) {
-    
+  MongoClient.connect(mongoDbURL,  { useUnifiedTopology: true }, function(err, dbClient) {
     var dataBaseName ='test';
-    const dbCollection = dbClient.db(dataBaseName).collection('accounts');
+    var dbCollection = dbClient.db(dataBaseName).collection('accounts');
     dbCollection.insert([
-      { _id: "hh", balance: 500, pendingTransactions: [] },
-      { _id: "dd", balance:500, pendingTransactions: [] }
-    ], function(error, result) {
-      if (error) {
-            // console.log("Got error");
-            // res.send("Error");
-            const dbCollection = dbClient.db(dataBaseName).collection('transactions');
-            dbCollection.insert(
-              { _id: 3 , source: "hh", destination: "dd", value:200, state: "initial", lastModified: new Date() }
-          )
-        }
-      });
-    /* var t = dbCollection.findOne( { state: "initial" } );
-    console.log(t)
-    var dateThreshold = new Date();
-    dateThreshold.setMinutes(dateThreshold.getMinutes() - 30); */
-   
+            { _id:5, name: "hh", balance: 500, pendingTransactions: [] },
+            { _id:6,name: "dd", balance:500, pendingTransactions: [] }
+            
+            ] )/* , function(error, result){ */
+          var dataBaseName ='test';
+          var dbCollection = dbClient.db(dataBaseName).collection('dbCollection');
+        const dbCollectTransaction = dbClient.db(dataBaseName).collection('transactions');
+    function cancel(id) {
+      dbCollectTransaction.updateOne(
+        { _id: id }, 
+        { $set: { state: "canceled" } }
+      );
+      }
+
+  function rollback(from, to,value, id) {
+    // Reverse debit
+    dbCollection.updateOne({
+      name: from, 
+      pendingTransactions: { $in: [id] }
+    }, {
+      $inc: { balance: value }, 
+      $pull: { pendingTransactions: id }
+    });  
+
+   // Reverse credit
+    dbCollection.updateOne({
+      name: to, 
+      pendingTransactions: { $in: [id] }
+    }, {
+      $inc: { balance: -value }, 
+      $pull: { pendingTransactions: id }
+    });  
+
+    cancel(id);
+  }
+
+function cleanup(from, to, id) {
+  // Remove the transaction ids
+  dbCollection.updateOne(
+    { name: from }, 
+    { $pull: { pendingTransactions: id } });
   
-  });
-  
+  // Remove the transaction ids
+  dbCollection.updateOne(
+    { name: to }, 
+    { $pull: { pendingTransactions: id } });
+
+  // Update transaction to committed
+  dbCollectTransaction.updateOne(
+    { _id: id }, 
+    { $set: { state: "done" } });
+}
+
+function executeTransaction(from, to, value) {
+  var objectId = require ('mongodb').ObjectID;
+  var transactionId =objectId();
+
+  dbCollectTransaction.insert({
+    _id:  transactionId , 
+    source: from, 
+    destination: to, 
+    value: value, 
+    state: "initial"
   });
 
+  var result = dbCollectTransaction.updateOne(
+    { _id: transactionId }, 
+    { $set: { state: "pending" } }
+  );
+
+  if (result.modifiedCount == 0) {
+    cancel(transactionId);
+    throw Error("Failed to move transaction " + transactionId + " to pending");
+  }
+
+    // Set up pending debit
+    result = dbCollection.updateOne({
+      name: from, 
+      pendingTransactions: { $ne: transactionId }, 
+      balance: { $gte: value }
+    }, {
+      $inc: { balance: -value }, 
+      $push: { pendingTransactions: transactionId }
+    });
+
+  if (result.modifiedCount == 0) {
+    rollback(from, to, value, transactionId);
+    throw Error("Failed to debit " + from + " account");
+  }
+
+  // Setup pending credit
+  result = dbCollection.updateOne({
+    name: to, 
+    pendingTransactions: { $ne: transactionId }
+  }, {
+    $inc: { balance: value }, 
+    $push: { pendingTransactions: transactionId }
+  });
+
+  if (result.modifiedCount == 0) {
+    rollback(from, to, value, transactionId);
+    throw Error("Failed to credit " + to + " account");
+  }
+
+  // Update transaction to committed
+  result = dbCollectTransaction.updateOne(
+    { _id: transactionId }, 
+    { $set: { state: "committed" } }
+  );
+
+  if (result.modifiedCount == 0) {
+    rollback(from, to, value, transactionId);
+    throw Error("Failed to move transaction " + transactionId + " to committed");
+  }
+
+  // Attempt cleanup
+  cleanup(from, to, transactionId);
+}
+
+executeTransaction("hh", "dd", 200);
+})
+  // });
+
+});
 app.listen(3051, () => {
     console.log("server is up on 3051");
   });
