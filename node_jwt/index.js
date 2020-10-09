626ee4e3c1dd96227fd63f79d47eddf488d1c33a
Hardi <hardi.technotery@gmail.com>
2020-10-09 18:27:49 +0530

    token verification

--- node_jwt/index.js
+++ node_jwt/index.js
@@ -9,11 +9,19 @@ app.get("/api", function(req,res){
     });
 });
 
-app.post('/api/posts',function(req,res){
-    res.json({
-        message: 'posting some value'
+app.post("/api/posts",verifyToken,function(req,res){
+    jwt.verify(req.token, "secretkey", function(err,authData){
+        if (err){
+            res.sendStatus(403); //forbidden
+        }else{
+            res.json({
+                message: 'posting some value',
+                authData,
+            });
+        }
     });
 });
+
 
 app.post("/api/login", function(req,res){
     const user ={
@@ -21,14 +29,23 @@ app.post("/api/login", function(req,res){
         username: "hardi",
         email: 'hardi@gmail.com',
     };
-    jwt.sign({user:user},"secretkey", function(err,token){
+    jwt.sign({user},"secretkey", function(err,token){
         res.json({
             token,
         });
     });
 });
 
-
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== "undefined"){
+        const bearerToken = bearerHeader.split("")[1];
+        req.token = bearerToken;
+        next();
+    }else{
+        res.sendStatus(403);
+    }
+}
 app.listen(3005, () => {
     console.log("server is up on 3005");
   });
