f2f0e8a372308a6ea3dc44cf497cec33fd88007e
Hardi <hardi.technotery@gmail.com>
2020-10-26 18:51:41 +0530

    posting data in angular using node-express

---
+++ angular_demo/angular_nodeapi/angular4/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular4';
+  topics =['Angular' , 'React' , 'Vue'];
+  topicHasError = true;
+  // submitted = false; in html file add  after ngForm*ngIf ="!submitted"
+
+  userModel = new User('hardi' , 'hardi@gmail.com', 1236548544 ,'default','HArdi007');
+  
+  constructor (private _enrollmentService: EnrollmentService){}
+
+  validateTopic(value){
+    if(value === 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit(){
+    // this.submitted = true;
+   this._enrollmentService.enroll(this.userModel)
+   .subscribe(
+     data => console.log('sucess!!', data),
+     error => console.error('Error!!', error) 
+   )
+  }
+}
