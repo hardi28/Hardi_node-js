6b1360c53d9efd54f3c14039db7024ae33241506
Hardi <hardi.technotery@gmail.com>
2020-10-29 13:19:06 +0530

    register user using angular and mongodb

---
+++ angular_demo/angular_nodeapi/angular_auth/ngApp/src/app/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerUserData = {};
+  constructor(private  _auth: AuthService,
+    private _router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  registerUser(){
+    // ----------------registerUserData which we want to add in mongoDB-------------------------------------
+    this._auth.registerUser(this.registerUserData)
+    .subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
+  }
+}
